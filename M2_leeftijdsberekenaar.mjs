b4eda9204d0dc10ad566ad844943cdfc47eb6f4f
Souhailbnl <souhailbnl@hotmail.com>
2024-09-24 11:54:25 +0200

    oefeningen

---
+++ M2_leeftijdsberekenaar.mjs
@@ -0,0 +1,11 @@
+let geboorteJaar = 2001;
+let toekomstJaar = 2031;
+
+let leeftijds1;
+let leeftijds2;
+
+leeftijds1 = toekomstJaar - geboorteJaar;
+leeftijds2 = toekomstJaar - geboorteJaar - 1;
+
+console.log("In " + toekomstJaar + " " + "zal ik " + leeftijds1 + " " + "of " + leeftijds2 + " " + "zijn." )
+
